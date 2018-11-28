e663bd9d1fc8a2ee674f6144c8bd3ae5f9b74cd6
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2018-11-28 16:24:22 -0500

    first commit

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Update latest Roles
+rm -rf roles/*
+git clone https://github.com/redhat-gpte-devopsautomation/ansible-operator-roles
+cp -R ansible-operator-roles/roles/postgresql-ocp ./roles
+cp -R ansible-operator-roles/roles/gitea-ocp ./roles
+cp ansible-operator-roles/playbooks/gitea.yaml ./playbook.yaml
+rm -rf ansible-operator-roles
+
+# Now build the Operator
+operator-sdk build quay.io/wkulhanek/gitea-operator:v0.0.1
+docker push quay.io/wkulhanek/gitea-operator:v0.0.1
