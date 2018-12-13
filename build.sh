17b22682725b26904a65533a1e946c8c63c840f1
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2018-12-13 17:45:38 -0500

    Updated to 0.0.2

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ cp ansible-operator-roles/playbooks/gitea.yaml ./playbook.yaml
 rm -rf ansible-operator-roles
 
 # Now build the Operator
-operator-sdk build quay.io/wkulhanek/gitea-operator:v0.0.1
-docker push quay.io/wkulhanek/gitea-operator:v0.0.1
+operator-sdk build quay.io/wkulhanek/gitea-operator:v0.0.2
+docker push quay.io/wkulhanek/gitea-operator:v0.0.2
